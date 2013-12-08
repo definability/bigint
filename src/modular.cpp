c93efebb961632641eedc33cfcd46f16f924aaa4
char-lie <5962274+char-lie@users.noreply.github.com>
2013-12-08 13:41:27 +0200

    Bugs fixed: incorrect 0 in output

--- src/modular.cpp
+++ src/modular.cpp
@@ -7,30 +7,45 @@
 void pow_mod(t_bint* a, t_bint* b, t_bint* n, t_size sizeA, t_size sizeB, t_size sizeN) {
     if (!sizeB || sizeB>sizeA) sizeB=sizeA;
     if (!sizeN) sizeN=sizeA;
+
+    if (isNull(a,sizeA)) {
+        return;
+    }
+    if (isNull(b,sizeB)) {
+        setNull(a,sizeA);
+        a[0]=1;
+        return;
+    }
+
     t_size sizeT = sizeN*2;
     t_size sizeR = sizeT;
     t_bint* tmp = new t_bint[sizeT];
     t_bint* result = new t_bint[sizeR];
     t_size sizeP = msw(b,sizeB)+1;
     t_bint* p = new t_bint[sizeP];
+
     setNull(p,sizeP);
     setNull(tmp,sizeT);
     setNull(result,sizeR);
+
     result[0]=1;
     mod(a,n,sizeA,sizeN);
     mov(p,b,sizeP);
-    if (isNull(a,sizeA)) return;
-    mov(tmp,a,sizeN);
+    mov(tmp,a,sizeA);
+
     while (!isNull(p,sizeP)) {
         if (p[0]&1) {
             mul(result,tmp,sizeR,sizeT);
             mod(result,n,sizeR,sizeN);
         }
         mul(tmp,tmp,sizeT);
+        mod(tmp,n,sizeT,sizeN);
         shr(p,1,sizeP);
     }
     mov(a,result,sizeA);
     mod(a,n,sizeA,sizeN);
+
     delete[] tmp;
     delete[] result;
+    delete[] p;
 }
