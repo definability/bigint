0452fb555aa10aee00e7c2d6bbf85e905b3f2ff2
char-lie <5962274+char-lie@users.noreply.github.com>
2013-12-15 18:52:15 +0200

    Added USAGE (--help)

--- tests/time.cpp
+++ tests/time.cpp
@@ -25,9 +25,16 @@ int cpp_main(int argc, char* argv[]) {
     t_size test_number;
 
     if (argc < 3) {
-        cerr << "Must be at least 2 arguments: test_number repeat_times [timeout_sec]" << endl;
-        printHelp();
-        return 1;
+        if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+            printHelp(argv[0]);
+            return 0;
+        }
+        else {
+            cerr << "Must be at least 2 arguments" << endl;
+            cerr << "Use --help to see the usage" << endl;
+            printHelp(argv[0]);
+            return 1;
+        }
     }
     else {
         test_number = getArgv(argv[1]);
@@ -57,11 +64,28 @@ int cpp_main(int argc, char* argv[]) {
     return result;
 }
 
-void printHelp() {
-    cout << "Available tests:" << endl;
+void printHelp(const char programName[]) {
+    cout << endl;
+    cout << "USAGE: " << programName << " TEST_NUMBER REPEAT_TIMES [TIMEOUT_SEC=10]" << endl;
+    cout << endl;
+    cout << "  TEST_NUMBER  is a number of the test from the following list:" << endl;
     for (t_size i = 0; i < TESTS_NUMBER; i++ ) {
-        cout << i + 1 << ") " << tests[i] << endl;
+        cout << "    [" << i + 1 << "] " << tests[i] << endl;
     }
+    cout << endl;
+    cout << "  REPEAT_TIMES is a number of iterations of every test" << endl;
+    cout << endl;
+    cout << "  TIMEOUT_SEC  is a time in seconds what you are ready to wait." << endl;
+    cout << "               Standard value is 10" << endl;
+    cout << endl;
+    cout << "  EXAMPLES:" << endl;
+    cout << "    " << programName << " 1 1000000" << endl;
+    cout << "        will cause million times BigInt generation" << endl;
+    cout << endl;
+    cout << "    " << programName << " 6 10 100" << endl;
+    cout << "        will cause ten times BigInt power by modulus and will be" << endl;
+    cout << "        aborted if the timeout will reach the 100 seconds barier" << endl;
+    cout << endl;
 }
 
 long getArgv(char argv[]) {
--- tests/time.h
+++ tests/time.h
@@ -1,3 +1,3 @@
 int main(int argc, char* argv[]);
 t_size getArgv(char argv[]);
-void printHelp();
+void printHelp(const char programName[] = "time");
